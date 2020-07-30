2f5babe6274c68eed4eb0701b2160b9d2d866ffb
Kareem Hisham <thekareem13@gmail.com>
2020-07-30 18:31:37 +0200

    Added header text & background

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import "./fonts/african-culture-font/AfricanCulture-J17a.ttf";
+import "./fonts/afrika-safari-rebuild-st-font/AfrikaSafariRebuildSt-2lzw.ttf";
+import "./fonts/afrika-t-ubuntu-font/AfrikaTUbuntu-nAOV.ttf";
+import "./fonts/afrika-wildlife-b-mammals2-font/AfrikaWildlifeBMammals2-9MJj.ttf";
+import "./fonts/egyptian-nights-font/EgyptianNights-yAOV.ttf";
+import "./fonts/tajamuka-script-font/TajamukaScript-ByW5.ttf";
+import "./fonts/tribeca-font/TribecaRegular-d9P6.ttf";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
---
+++ src/pages/home.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="section-header">
+      <div className="header">
+        <div className="header__text">
+          <h1 className="header__heading">
+            7 Reasons why Zamalek is the{" "}
+            <span className="header__span">African club of the century</span>
+          </h1>
+        </div>
+        <div className="header__logo"></div>
+      </div>
+    </div>
+  );
+}
