d34094ee339b305032775bcc9cd08c8ab6bd6c0a
Kareem Hisham <thekareem13@gmail.com>
2020-08-02 15:14:12 +0200

    finished section 2

--- src/pages/nine-seven.js
+++ src/pages/nine-seven.js
@@ -9,25 +9,43 @@ export default function NineSeven() {
       <div className="container">
         <div className="column column-one">
           <div className="card">
-            <div class="card__side card__side--front">COLUMN ONE ZAMALEK</div>
-            <div class="card__side card__side--back card__side--back-1">
-              COLUMN ONE AHLY
+            <div class="card__side card__side--front">
+              <div class="card__picture card__picture--1">&nbsp;</div>
+              <div className="card__heading">
+                Zamalek has won 4 Champions league trophies
+              </div>
+            </div>
+            <div class="card__side card__side--back">
+              <div class="card__picture card__picture--2">&nbsp;</div>
+              <div className="card__heading">Ahly has only won two</div>
             </div>
           </div>
         </div>
         <div className="column column-two">
           <div className="card">
-            <div class="card__side card__side--front">COLUMN TWO ZAMALEK</div>
-            <div class="card__side card__side--back card__side--back-2">
-              COLUMN TWO AHLY
+            <div class="card__side card__side--front">
+              <div class="card__picture card__picture--3">&nbsp;</div>
+              <div className="card__heading">
+                Zamalek has won 2 african super cups
+              </div>
+            </div>
+            <div class="card__side card__side--back">
+              <div class="card__picture card__picture--4">&nbsp;</div>
+              <div className="card__heading">Ahly has none</div>
             </div>
           </div>
         </div>
         <div className="column column-three">
           <div className="card">
-            <div class="card__side card__side--front">COLUMN THREE ZAMALEK</div>
-            <div class="card__side card__side--back card__side--back-3">
-              COLUMN THREE AHLY
+            <div class="card__side card__side--front">
+              <div class="card__picture card__picture--5">&nbsp;</div>
+              <div className="card__heading">
+                Zamalek has won a total of 9 trophies
+              </div>
+            </div>
+            <div class="card__side card__side--back">
+              <div class="card__picture card__picture--6">&nbsp;</div>
+              <div className="card__heading">Ahly has won a total of 7</div>
             </div>
           </div>
         </div>
